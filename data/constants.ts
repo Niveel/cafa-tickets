8f6a7ef994118ad9b0ff7a108278715328aa95f4
klasique-art <feboapong@gmail.com>
2025-12-19 07:19:52 +0000

    switched base url

--- data/constants.ts
+++ data/constants.ts
@@ -2,16 +2,9 @@ export const placeholderPic = "https://images.unsplash.com/vector-1738925788108-
 
 export const placeholderImage = "https://images.unsplash.com/photo-1559551538-96c62c56b256?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-export const BASE_URL = "http://localhost:8000/api/v1"
+export const BASE_URL = process.env.NODE_ENV === "production"
+        ? "https://cafaticket.pythonanywhere.com/api/v1"
+        : "http://localhost:8000/api/v1";
 
-// signup endpoint = /auth/users/
-// login endpoint = /auth/login/
-
-/**
- * 
- * {
-    "username": "Klasique2",
-    "email": "feboapong2@gmail.com",
-    "password": "Klasique2"
-}
- */
+// "https://cafaticket.pythonanywhere.com/api/v1"
+// export const BASE_URL = "http://localhost:8000/api/v1"
